3d2e1392b5d9a88778df9ddcb017acfef8a6b465
Alexander <alexgomezgomez15@gmail.com>
2023-06-29 17:36:34 -0400

    Lab Completed

--- lab-answers/lab-answers.js
+++ lab-answers/lab-answers.js
@@ -1,93 +1,93 @@
-// //============Easy going=========
+//============Easy going=========
 
-// for (let i = 0; i <=20; i++){
-//     console.log(i);
-// }
+for (let i = 0; i <=20; i++){
+    console.log(i);
+}
 
-// //=============Get Even============
+//=============Get Even============
 
-// for (let i = 2; i <= 200; i+=2) {
-//     console.log(i);
-// }
+for (let i = 2; i <= 200; i+=2) {
+    console.log(i);
+}
 
-// //==============Fizz Buzz==============
-// for (let i = 1; i <= 100; i++){
-//     if(i % 3 === 0 && i % 5 ===0){
-//         console.log("FizzBuzz");
-//     }else if(i % 3 === 0){
-//         console.log("Fizz");
-//     }else if(i % 5 === 0){
-//         console.log("Buzz");
-//     }else{
-//         console.log(i);
-//     }
-// }
+//==============Fizz Buzz==============
+for (let i = 1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 ===0){
+        console.log("FizzBuzz");
+    }else if(i % 3 === 0){
+        console.log("Fizz");
+    }else if(i % 5 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
 
-// //=========Wild Wild Life =================
-// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-// const sharky = ["Sharky", "shark", 20, "Left Coast"];
-// const plantee = ["Plantee", "plant",  5000 , "Mordor"];
-// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+//=========Wild Wild Life =================
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
 
-// plantee[2] = 5001;
-// console.log(plantee[2]);
+plantee[2] = 5001;
+console.log(plantee[2]);
 
-// wolfy[3] = "Gotham City"
-// console.log(wolfy[3]);
+wolfy[3] = "Gotham City"
+console.log(wolfy[3]);
 
-// dart.push("Hawkins");
-// console.log(dart[dart.length -1]);
+dart.push("Hawkins");
+console.log(dart[dart.length -1]);
 
-// wolfy.splice(0, 1 , "Gameboy");
-// console.log(wolfy[0]);
+wolfy.splice(0, 1 , "Gameboy");
+console.log(wolfy[0]);
 
-// //=======Yell at the Ninjag Turtles===============
-// const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+//=======Yell at the Ninjag Turtles===============
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
 
-// for (let turtle of ninjaTurtles) {
-//     console.log(turtle.toUpperCase());
-// }
+for (let turtle of ninjaTurtles) {
+    console.log(turtle.toUpperCase());
+}
 
-// //===============Methods Revisited====================
-// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+//===============Methods Revisited====================
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
-// //Console log the index of Titanic.
-// console.log(favMovies[8]);
+//Console log the index of Titanic.
+console.log(favMovies[8]);
 
-// //Use the .sort method
-// console.log(favMovies.sort());
+//Use the .sort method
+console.log(favMovies.sort());
 
 
-// //push "Guardians of the Galaxy".
-// favMovies.push("Guardians of the Galaxy");
+//push "Guardians of the Galaxy".
+favMovies.push("Guardians of the Galaxy");
 
-// //Use the method pop.
-// favMovies.pop();
-// console.log(favMovies);
+//Use the method pop.
+favMovies.pop();
+console.log(favMovies);
 
-// //Reverse the array.
-// console.log(favMovies.reverse());
+//Reverse the array.
+console.log(favMovies.reverse());
 
-// //Use the shift method.
-// favMovies.shift();
-// console.log(favMovies);
+//Use the shift method.
+favMovies.shift();
+console.log(favMovies);
 
-// //unshift - what does it return?
-// favMovies.unshift("Toy Story");
-// console.log(favMovies);
+//unshift - what does it return?
+favMovies.unshift("Toy Story");
+console.log(favMovies);
 
-// //splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
+//splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
 
-// favMovies.splice(favMovies.indexOf("Django Unchained"), 1, "Avatar");
+favMovies.splice(favMovies.indexOf("Django Unchained"), 1, "Avatar");
 
-// console.log(favMovies);
+console.log(favMovies);
 
-// //slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
-// let slicedArr = favMovies.slice(favMovies.length/2);
-// console.log(slicedArr);
+//slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+let slicedArr = favMovies.slice(favMovies.length/2);
+console.log(slicedArr);
 
-// //After running the above tasks, console.log the index of "Fast and Furious."
+//After running the above tasks, console.log the index of "Fast and Furious."
 // console.log(favMovies.indexOf("Fast and Furious"));
 
 //=================Where is Waldo======================
