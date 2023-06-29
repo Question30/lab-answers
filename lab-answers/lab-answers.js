974fb6008be450d5f787920fe1e647d85e396d04
Alexander <alexgomezgomez15@gmail.com>
2023-06-29 17:12:49 -0400

    Yell at  the Ninja Turtles answered

--- lab-answers/lab-answers.js
+++ lab-answers/lab-answers.js
@@ -23,21 +23,28 @@
 //     }
 // }
 
-//=========Wild Wild Life =================
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant",  5000 , "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+// //=========Wild Wild Life =================
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
 
-plantee[2] = 5001;
-console.log(plantee[2]);
+// plantee[2] = 5001;
+// console.log(plantee[2]);
 
-wolfy[3] = "Gotham City"
-console.log(wolfy[3]);
+// wolfy[3] = "Gotham City"
+// console.log(wolfy[3]);
 
-dart.push("Hawkins");
-console.log(dart[dart.length -1]);
+// dart.push("Hawkins");
+// console.log(dart[dart.length -1]);
 
-wolfy.splice(0, 1 , "Gameboy");
-console.log(wolfy[0]);
+// wolfy.splice(0, 1 , "Gameboy");
+// console.log(wolfy[0]);
+
+//=======Yell at the Ninjag Turtles===============
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+
+for (let turtle of ninjaTurtles) {
+    console.log(turtle.toUpperCase());
+}
