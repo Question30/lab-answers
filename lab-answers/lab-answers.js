3b5c2a57a0717d3d4199a1b4bde3ca59d51d6e6b
Alexander <alexgomezgomez15@gmail.com>
2023-06-29 16:52:58 -0400

    Get Even answered

--- lab-answers/lab-answers.js
+++ lab-answers/lab-answers.js
@@ -1,5 +1,11 @@
-//============Easy going=========
+// //============Easy going=========
 
-for (let i = 0; i <=20; i++){
+// for (let i = 0; i <=20; i++){
+//     console.log(i);
+// }
+
+//=============Get Even============
+
+for (let i = 2; i <= 200; i+=2) {
     console.log(i);
 }
