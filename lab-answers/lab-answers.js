df7c5d88627094d4b9628267342f0c7d8fba064c
Alexander <alexgomezgomez15@gmail.com>
2023-06-29 17:34:38 -0400

    Where is Waldo answered

--- lab-answers/lab-answers.js
+++ lab-answers/lab-answers.js
@@ -49,43 +49,59 @@
 //     console.log(turtle.toUpperCase());
 // }
 
-//===============Methods Revisited====================
-const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// //===============Methods Revisited====================
+// const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
-//Console log the index of Titanic.
-console.log(favMovies[8]);
+// //Console log the index of Titanic.
+// console.log(favMovies[8]);
 
-//Use the .sort method
-console.log(favMovies.sort());
+// //Use the .sort method
+// console.log(favMovies.sort());
 
 
-//push "Guardians of the Galaxy".
-favMovies.push("Guardians of the Galaxy");
+// //push "Guardians of the Galaxy".
+// favMovies.push("Guardians of the Galaxy");
 
-//Use the method pop.
-favMovies.pop();
-console.log(favMovies);
+// //Use the method pop.
+// favMovies.pop();
+// console.log(favMovies);
 
-//Reverse the array.
-console.log(favMovies.reverse());
+// //Reverse the array.
+// console.log(favMovies.reverse());
 
-//Use the shift method.
-favMovies.shift();
-console.log(favMovies);
+// //Use the shift method.
+// favMovies.shift();
+// console.log(favMovies);
 
-//unshift - what does it return?
-favMovies.unshift("Toy Story");
-console.log(favMovies);
+// //unshift - what does it return?
+// favMovies.unshift("Toy Story");
+// console.log(favMovies);
 
-//splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
+// //splice "Django Unchained" and add "Avatar" (try finding the index of "Django Unchained" instead of counting it yourself).
 
-favMovies.splice(favMovies.indexOf("Django Unchained"), 1, "Avatar");
+// favMovies.splice(favMovies.indexOf("Django Unchained"), 1, "Avatar");
 
-console.log(favMovies);
+// console.log(favMovies);
 
-//slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
-let slicedArr = favMovies.slice(favMovies.length/2);
-console.log(slicedArr);
+// //slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+// let slicedArr = favMovies.slice(favMovies.length/2);
+// console.log(slicedArr);
 
-//After running the above tasks, console.log the index of "Fast and Furious."
-console.log(favMovies.indexOf("Fast and Furious"));
+// //After running the above tasks, console.log the index of "Fast and Furious."
+// console.log(favMovies.indexOf("Fast and Furious"));
+
+//=================Where is Waldo======================
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                      ["Lucinda", "Jacc", "Neff", "Snoop"],
+                      ["Petunia", ["Baked Goods", "Waldo"]]];
+
+//Remove Eggbert (hint look at the slice/splice methods).
+whereIsWaldo.splice(1, 1);
+console.log(whereIsWaldo);
+
+//Change "Neff" to "No One".
+whereIsWaldo[1][2] = "No One";
+console.log(whereIsWaldo);
+
+//Access and console.log "Waldo".
+console.log(whereIsWaldo[2][1][1]);
